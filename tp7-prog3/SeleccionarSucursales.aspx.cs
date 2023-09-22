b9f9b8a120daedd27c15e3401453337dc4d74b49
LoreVeren <Loreverenzuela@gmail.com>
2023-09-22 17:05:16 -0300

    Agrego la sucursales seleccionadas en la variable Session

--- tp7-prog3/SeleccionarSucursales.aspx.cs
+++ tp7-prog3/SeleccionarSucursales.aspx.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
-
+using System.Data;
 namespace tp7_prog3
 {
     public partial class SeleccionarSucursales : System.Web.UI.Page
@@ -28,11 +28,42 @@ namespace tp7_prog3
         {
             if (e.CommandName == "eventoSeleccionar")
             {
-                Session["DatosSuc"] = e.CommandArgument.ToString();
-               
+                if (Session["DatosSuc"] == null)
+                {
+                    Session["DatosSuc"] = CrearTabla();
+                }
+                string[] valores = e.CommandArgument.ToString().Split(' ');
+                agregarFila((DataTable)Session["DatosSuc"], valores);
             }
         }
+        private DataTable CrearTabla()
+        {
+            DataTable dt = new DataTable();
 
+            dt.Columns.Add(new DataColumn("ID_Sucursal", Type.GetType("System.String")));
+            dt.Columns.Add(new DataColumn("Nombre", Type.GetType("System.String")));
+            dt.Columns.Add(new DataColumn("Descripcion", Type.GetType("System.String")));
+
+            return dt;
+        }
+        public void agregarFila(DataTable tabla, string[] valores)
+        {
+            if (tabla.Rows.Count != 0)
+            {
+                foreach (DataRow fila in tabla.Rows)
+                {
+                    if (fila["Id_Sucursal"].ToString() == valores[0])
+                    {
+                        return;
+                    }
+                }
+            }
+            DataRow dr = tabla.NewRow();
+            dr["ID_Sucursal"] = valores[0];
+            dr["Nombre"] = valores[1];
+            dr["Descripcion"] = valores[2];
+            tabla.Rows.Add(dr);
+        }
         protected void btnProvincias_Command(object sender, CommandEventArgs e)
         {
             if (e.CommandName == "BtnProvinciaID")
